b83f9aa5cef241286ff5b921f53c47b109f6472d
openairs <103561980+openairs@users.noreply.github.com>
2022-10-26 11:19:36 +0530

    Add files via upload

---
+++ Preventing Application from Sensitive Information Disclosure through Custom Error pages/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorHandlingApp.Controllers
+{
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        public IEnumerable<string> Get()
+        {
+            throw new Exception("Parameter value cannot be null");
+            return new string[] {"value1", "value2"};
+        }
+
+        // GET: ValuesController/Details/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: ValuesController/Create
+        
+        public void Post([FromBody]string value)
+        {
+            
+        }
+
+        // POST: ValuesController/Edit/5
+        
+        public void Put(int id, [FromBody]string value)
+        {
+           
+        }
+
+        // DELETE: ValuesController/Delete/5
+        public void Delete(int id)
+        {
+            
+        }
+
+        // POST: ValuesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Preventing Application from Sensitive Information Disclosure through Custom Error pages/Models/ExceptionFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Web.Http.Filters;
+using ExceptionFilterAttribute = System.Web.Http.Filters.ExceptionFilterAttribute;
+
+namespace ErrorHandlingApp.Models
+{
+    public class NotImpExceptionFilterAttribute:ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if(context.Exception != null)
+            {
+                var exception = context.Exception;
+                var message = context.Exception.Message;
+                var innerMessage = context.Exception.InnerException;
+                var path = context.Request.RequestUri.AbsolutePath;
+            }
+        }
+    }
+}
